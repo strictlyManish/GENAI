d6edd54f8a9dbb536b789389f10b15fc39bde2ff
strictlyManish <rajzmanish94@gmail.com>
2026-04-04 20:43:00 +0530

    Refactor app.js for improved readability and organization of middleware and routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,32 +6,28 @@ const cors = require("cors");
 const InterviewRoute = require("./routes/interview.routes");
 const path = require("path");
 
-
 // Middlewares
 app.use(express.json());
 app.use(cookie_parser());
 app.use(express.static("public"));
 
-
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-  }),
+  })
 );
 
-
-//API
+// API routes
 app.use("/api/auth", authRouter);
 app.use("/api/interview", InterviewRoute);
 
-
-
+// Serve frontend build
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+// Catch-all route (must come last)
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
-
 
 module.exports = app;
